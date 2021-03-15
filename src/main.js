c0063982e0640960a066bdf0455e5e4e24ea38b8
rh-pr <ruslana.hural.j@gmail.com>
2021-03-15 20:50:29 +0200

    exmp with routing

--- src/main.js
+++ src/main.js
@@ -1,22 +1,46 @@
-//'use strict'
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 8000;
+const user = { name: 'jura', age: 22 };
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello World\n');
-});
+const routing = {
+  '/': '<h1>welcome to homepage</h1><hr>',
+  '/user': user,
+  '/user/name': () => user.name.toUpperCase(),
+  '/user/age': () => user.age,
+  '/hello': { hello: 'world', andArray: [1, 2, 3, 4, 5, 6, 7] },
+  '/api/method1': (req, res) => {
+    console.log(`${req.url}  ${res.statusCode}`);
+    return { status: res.statusCode };
+  },
+  '/api/method2': req => ({
+    user,
+    url: req.url,
+    cookie: req.headers.cookie
+  }),
+};
 
-server.listen(port,hostname,() => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const types = {
+  object: JSON.stringify,
+  string: s => s,
+  undefined: () => 'not found',
+  function: (fn, req, res) => JSON.stringify(fn(req, res)),
+};
 
-server.on('error', err => {
-    if (err.code === 'EACCES') {
-        console.log(`No access to port: ${port}`)
-    }
-});
+http.createServer((req, res) => {
+  const data = routing[req.url];
+  const type = typeof data;
+  const serializer = types[type];
+  const result = serializer(data, req, res);
+  res.end(result);
+}).listen(8001);
+
+// http.createServer((req, res) => {
+//   const data = routing[req.url];
+//   res.end(types[typeof data](data, req, res));
+// }).listen(8000);
+
+setInterval(() => { 
+    user.age += 1;
+    return user.age;
+ }, 2000);
